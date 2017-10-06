8374a9f2f64c8f7080d26b411be8d77e364345eb
luznyjak <luznyjak@WL302954.eu.tieto.com>
2017-10-06 15:56:16 +0200

    aaaaaakuiku

--- pipeline/VM_Deploy_Ansible.groovy
+++ pipeline/VM_Deploy_Ansible.groovy
@@ -6,8 +6,8 @@ node {
 		git credentialsId: '513094ca-73b7-4145-8829-4474bbbb1c24', url: 'git@github.com:xelor81/us000832.git'
 		//**************************************************************
 	}
-	dir("pipeline")
-	def stages = load "./common_functions.groovy";
+	
+	def stages = load "./pipeline/common_functions.groovy";
 	
 	println("------- ANSIBLE PART --------")
 	stages.deploy_VM_NET()
--- pipeline/common_functions.groovy
+++ pipeline/common_functions.groovy
@@ -3,7 +3,7 @@ def deploy_VM_NET() {
 		ansiblePlaybook colorized: true,
 		extras: 'NETWORK_NAME=Isolated1,Isolated2,Isolated3',
 		installation: 'Ansible 2.1.2.0',
-		playbook: '../Ansible/Playbooks/VM/define-VM-net.yml',
+		playbook: './Ansible/Playbooks/VM/define-VM-net.yml',
 		sudo: true
 	}
 
@@ -14,7 +14,7 @@ def deploy_VM_LVM(VOL_NAME) {
 		ansiblePlaybook colorized: true,
 		extras: 'VG_NAME=kvm-vms VOL_NAME=${VOL_NAME}',
 		installation: 'Ansible 2.1.2.0',
-		playbook: '../Ansible/Playbooks/VM/define-VM_LVM.yml',
+		playbook: './Ansible/Playbooks/VM/define-VM_LVM.yml',
 		sudo: true
 	}
 
@@ -26,7 +26,7 @@ def deploy_VM_ans(VM_NAME, CPU, MEMORY, VM_NETWORK) {
 		ansiblePlaybook colorized: true, 
 		extras: "CPU=2 MEMORY=1024 VM_STORAGE=/tmp/zz VM_NAME=desktop5 VM_NETWORK=bridge,Isolated2,Isolated3,Isolated1", 
 		installation: 'Ansible 2.1.2.0', 
-		playbook: '../Ansible/Playbooks/VM/defineVM.yml', 
+		playbook: './Ansible/Playbooks/VM/defineVM.yml', 
 		sudo: true
 	}
 
