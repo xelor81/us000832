43e96ccb5035d2c2ce7e2a940450942e3ac0bc53
luznyjak <luznyjak@WL302954.eu.tieto.com>
2017-10-06 15:57:58 +0200

    zzz

--- pipeline/common_functions.groovy
+++ pipeline/common_functions.groovy
@@ -1,7 +1,7 @@
 def deploy_VM_NET() {
 	stage('Defining VM Networks and activating them') {
 		ansiblePlaybook colorized: true,
-		extras: 'NETWORK_NAME=Isolated1,Isolated2,Isolated3',
+		extras: '-e NETWORK_NAME=Isolated1,Isolated2,Isolated3',
 		installation: 'Ansible 2.1.2.0',
 		playbook: './Ansible/Playbooks/VM/define-VM-net.yml',
 		sudo: true
@@ -12,7 +12,7 @@ def deploy_VM_NET() {
 def deploy_VM_LVM(VOL_NAME) {
 	stage('Defining VM Storage') {
 		ansiblePlaybook colorized: true,
-		extras: 'VG_NAME=kvm-vms VOL_NAME=${VOL_NAME}',
+		extras: '-e VG_NAME=kvm-vms VOL_NAME=${VOL_NAME}',
 		installation: 'Ansible 2.1.2.0',
 		playbook: './Ansible/Playbooks/VM/define-VM_LVM.yml',
 		sudo: true
@@ -24,7 +24,7 @@ def deploy_VM_LVM(VOL_NAME) {
 def deploy_VM_ans(VM_NAME, CPU, MEMORY, VM_NETWORK) {
 	stage('Deploying VM') {
 		ansiblePlaybook colorized: true, 
-		extras: "CPU=2 MEMORY=1024 VM_STORAGE=/tmp/zz VM_NAME=desktop5 VM_NETWORK=bridge,Isolated2,Isolated3,Isolated1", 
+		extras: "-e CPU=2 MEMORY=1024 VM_STORAGE=/tmp/zz VM_NAME=desktop5 VM_NETWORK=bridge,Isolated2,Isolated3,Isolated1", 
 		installation: 'Ansible 2.1.2.0', 
 		playbook: './Ansible/Playbooks/VM/defineVM.yml', 
 		sudo: true
