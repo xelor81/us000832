2abe2f4c0e58f08c0882ffc29d68313b063a9e38
luznyjak <luznyjak@WL302954.eu.tieto.com>
2017-10-06 15:27:40 +0200

    initial draft for ansible

---
+++ pipeline/VM_Deploy_Ansible.groovy
@@ -0,0 +1,12 @@
+node {
+	env.WORKSPACE_ROOT_DIR = env.WORKSPACE.substring(0, env.WORKSPACE.lastIndexOf('/'));
+	
+	stage('Checkout Github') {
+		//**************************************************************
+		git credentialsId: '513094ca-73b7-4145-8829-4474bbbb1c24', url: 'git@github.com:xelor81/us000832.git'
+		//**************************************************************
+	}
+	def stages = load "./common_functions.groovy";
+	
+	stages.deploy_VM_NET()
+}
---
+++ pipeline/common_functions.groovy
@@ -0,0 +1,31 @@
+def deploy_VM_NET() {
+	stage('Defining VM Networks and activating them') {
+		ansiblePlaybook colorized: true,
+		extras: 'NETWORK_NAME=Isolated1,Isolated2,Isolated3',
+		installation: 'Ansible 2.1.2.0',
+		playbook: '../Ansible/Playbooks/VM/define-VM-net.yml',
+		sudo: true
+	}
+}
+
+def deploy_VM_LVM(VOL_NAME) {
+	stage('Defining VM Storage') {
+		ansiblePlaybook colorized: true,
+		extras: 'VG_NAME=kvm-vms VOL_NAME=${VOL_NAME}',
+		installation: 'Ansible 2.1.2.0',
+		playbook: '../Ansible/Playbooks/VM/define-VM_LVM.yml',
+		sudo: true
+	}
+}
+
+
+def deploy_VM_ans(VM_NAME, CPU, MEMORY, VM_NETWORK) {
+	stage('Deploying VM') {
+		ansiblePlaybook colorized: true, 
+		extras: "CPU=2 MEMORY=1024 VM_STORAGE=/tmp/zz VM_NAME=desktop5 VM_NETWORK=bridge,Isolated2,Isolated3,Isolated1", 
+		installation: 'Ansible 2.1.2.0', 
+		playbook: '../Ansible/Playbooks/VM/defineVM.yml', 
+		sudo: true
+	}
+}
+
