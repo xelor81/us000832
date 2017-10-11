86d4875c5aaaa146927014fd162689458d877e11
luznyjak <luznyjak@WL302954.eu.tieto.com>
2017-10-11 12:01:31 +0200

    ansible colors fix

--- pipeline/VM_Deploy_Ansible.groovy
+++ pipeline/VM_Deploy_Ansible.groovy
@@ -1,15 +1,14 @@
 node {
 	env.WORKSPACE_ROOT_DIR = env.WORKSPACE.substring(0, env.WORKSPACE.lastIndexOf('/'));
-	
-	stage('Checkout Github') {
-		//**************************************************************
-		git credentialsId: '513094ca-73b7-4145-8829-4474bbbb1c24', url: 'git@github.com:xelor81/us000832.git'
-		//**************************************************************
-	}
-	
 	def stages = load "./pipeline/common_functions.groovy";
 	
-	println("------- ANSIBLE PART --------")
+	git_credentials='513094ca-73b7-4145-8829-4474bbbb1c24'
+	git_url='git@github.com:xelor81/us000832.git'
+	git_branch='master'
+	
+	//------------------ EXECUTE PIPELINES JOBS ---------------------------
+	
+	stages.git_checkout(${git_branch}, ${git_login}, ${git_url})
 	stages.deploy_VM_NET()
 	
 }
--- pipeline/common_functions.groovy
+++ pipeline/common_functions.groovy
@@ -1,22 +1,30 @@
 def setting_ansible_color() {
-	stage("Setup Ansible Color schema for better console output") {
+	stage("***** Setup Ansible Color schema for better console output *****") {
 		export ANSIBLE_FORCE_COLOR=true
 	}
 }
 
-def deploy_VM_NET() {
-	stage('Defining VM Networks and activating them') {
-		ansiblePlaybook colorized: true,
-		extras: '-e NETWORK_NAME=Isolated1,Isolated2,Isolated3',
-		installation: 'Ansible 2.1.2.0',
-		playbook: './Ansible/Playbooks/VM/define-VM-net.yml',
-		sudo: true
+def git_checkout(git_branch, git_login, git_url) {
+	stage("***** Checkout Github *****") {
+		git branch: ${git_branch}, credentialsId: ${git_login}, url: ${git_url}
 	}
+		
+}
 
+def deploy_VM_NET() {
+	ansiColor('xterm') {
+		stage("***** Defining VM Networks and activating them *****") {
+			ansiblePlaybook colorized: true,
+			extras: '-e NETWORK_NAME=Isolated1,Isolated2,Isolated3',
+			installation: 'Ansible 2.1.2.0',
+			playbook: './Ansible/Playbooks/VM/define-VM-net.yml',
+			sudo: true
+		}
+	}
 }
 
 def deploy_VM_LVM(VOL_NAME) {
-	stage('Defining VM Storage') {
+	stage("***** Defining VM Storage *****") {
 		ansiblePlaybook colorized: true,
 		extras: '-e VG_NAME=kvm-vms VOL_NAME=${VOL_NAME}',
 		installation: 'Ansible 2.1.2.0',
@@ -28,7 +36,7 @@ def deploy_VM_LVM(VOL_NAME) {
 
 
 def deploy_VM_ans(VM_NAME, CPU, MEMORY, VM_NETWORK) {
-	stage('Deploying VM') {
+	stage("***** Deploying VM *****") {
 		ansiblePlaybook colorized: true, 
 		extras: "-e CPU=2 MEMORY=1024 VM_STORAGE=/tmp/zz VM_NAME=desktop5 VM_NETWORK=bridge,Isolated2,Isolated3,Isolated1", 
 		installation: 'Ansible 2.1.2.0', 
