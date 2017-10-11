1772cb403c2dc41493e8199163e839263fd5fcd8
luznyjak <luznyjak@WL302954.eu.tieto.com>
2017-10-11 12:48:56 +0200

    qaz

--- pipeline/VM_Deploy_Ansible.groovy
+++ pipeline/VM_Deploy_Ansible.groovy
@@ -1,5 +1,6 @@
 node {
 	env.WORKSPACE_ROOT_DIR = env.WORKSPACE.substring(0, env.WORKSPACE.lastIndexOf('/'));
+	
 	def stages = load "./pipeline/common_functions.groovy";
 	
 	git_login='513094ca-73b7-4145-8829-4474bbbb1c24'
@@ -11,9 +12,9 @@ node {
 	println("git LOGIN:  " + git_login)
 	println("git BRANCH: " + git_branch)
 	
-	stages.ZPRINT()
+	//stages.ZPRINT()
 	//stages.gitCHECK(git_branch, git_login, git_url)
 	
-	//stages.deploy_VM_NET()
+	stages.deploy_VM_NET()
 	
 }
