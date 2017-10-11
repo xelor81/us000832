120729439504e3013484c6f8de3a340080b290f3
luznyjak <luznyjak@WL302954.eu.tieto.com>
2017-10-11 12:31:16 +0200

    zz

--- pipeline/VM_Deploy_Ansible.groovy
+++ pipeline/VM_Deploy_Ansible.groovy
@@ -2,14 +2,14 @@ node {
 	env.WORKSPACE_ROOT_DIR = env.WORKSPACE.substring(0, env.WORKSPACE.lastIndexOf('/'));
 	def stages = load "./pipeline/common_functions.groovy";
 	
-	git_credentials='513094ca-73b7-4145-8829-4474bbbb1c24'
+	git_login='513094ca-73b7-4145-8829-4474bbbb1c24'
 	git_url='git@github.com:xelor81/us000832.git'
 	git_branch='master'
 	
 	//------------------ EXECUTE PIPELINES JOBS ---------------------------
 	println("ZZZZZZZZ: " + git_url)
 	println("login: " + git_login)
-	stages.git_checkout(git_branch, env.git_login, git_url)
+	stages.git_checkout(git_branch, git_login, git_url)
 	stages.deploy_VM_NET()
 	
 }
